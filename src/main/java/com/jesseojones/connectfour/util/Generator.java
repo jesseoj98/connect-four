398759385db7e3df22371d5820377fbfa802ee1e
jesseojones <jesseoj98@gmail.com>
2022-09-30 07:43:40 -0500

    game board generation

--- src/main/java/com/jesseojones/connectfour/util/Generator.java
+++ src/main/java/com/jesseojones/connectfour/util/Generator.java
@@ -2,4 +2,18 @@ package com.jesseojones.connectfour.util;
 
 public class Generator {
 
+	private static final int GAME_BOARD_Y_AXIS = 6;
+
+	private static final int GAME_BOARD_X_AXIS = 7;
+
+	private static final int GAME_BOARD_SPACES = 42;
+
+	public char[] generateSimpleGameBoard() {
+		return new char[GAME_BOARD_SPACES];
+	}
+
+	public char[][] generateComplexGameBoard() {
+		return new char[GAME_BOARD_X_AXIS][GAME_BOARD_Y_AXIS];
+	}
+
 }
