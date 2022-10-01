038d5a8a955b82816fd3c508f069e8d57e00f8fa
Jesse Jones <jesseoj98@gmail.com>
2022-10-01 06:47:08 -0500

    up one simple row helper method

--- src/main/java/com/jesseojones/connectfour/util/Helper.java
+++ src/main/java/com/jesseojones/connectfour/util/Helper.java
@@ -2,12 +2,22 @@ package com.jesseojones.connectfour.util;
 
 public class Helper {
 
+	private static final int PREVIOUS_SIMPLE_ROW = 7;
+
 	public boolean isSpaceAlreadyOccupied(char space) {
 		return space == 'X' || space == 'O';
 	}
 
 	public boolean isSimpleSpaceAlreadyOccupied(char[] board, int space) {
 		return isSpaceAlreadyOccupied(board[space]);
+	}
+
+	private int upOneSimpleRow(int value) {
+		if (value < PREVIOUS_SIMPLE_ROW) {
+			return value;
+		} else {
+			return value - PREVIOUS_SIMPLE_ROW;
+		}
 	}
 
 }
