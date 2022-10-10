247e8b3ed4eec3080e0fb2182e9f2c7c4edc9951
Jesse Jones <jesseoj98@gmail.com>
2022-10-09 19:12:05 -0500

    validator logic

--- src/main/java/com/jesseojones/connectfour/util/Validator.java
+++ src/main/java/com/jesseojones/connectfour/util/Validator.java
@@ -4,11 +4,41 @@ public class Validator {
 
 	private static final int BESIDE = 1;
 
+	private static final int CEILING = 6;
+
 	private static final int FORWARD_DIAGONAL = 6;
 
 	private static final int LEVEL = 7;
 
 	private static final int BACKWARD_DIAGONAL = 8;
+
+	private static final int FLOOR = 35;
+
+	private boolean connectFour(char[] board, int space) {
+		if (space > CEILING) {
+			// test below adjacent space(s)
+		}
+		if (space < FLOOR) {
+			// test above adjacent spaces(s)
+		}
+		if (space % LEVEL == 0) {
+			if (valueMatch(board[space], board[retrieveRightAdjacentSpace(space)])) {
+				// test right adjacent space(s)
+			}
+		}
+		if (space % LEVEL == 6) {
+			if (valueMatch(board[space], board[retrieveLeftAdjacentSpace(space)])) {
+				// test left adjacent space(s)
+			}
+		}
+		if (space > CEILING && space % LEVEL == 0) {
+			// test bottom-right adjacent space(s)
+		}
+		if (space < FLOOR && space % LEVEL == 6) {
+			// test above-right adjacent space(s)
+		}
+		return false;
+	}
 
 	private int retrieveTopAdjacentSpace(int value) {
 		return value + LEVEL;
