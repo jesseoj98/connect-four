208b1b331078790550ea0c7bf790a0cda5487dc3
jesseojones <jesseoj98@gmail.com>
2022-12-11 16:19:02 -0600

    remove 2D implementation of game board

--- src/main/java/com/jesseojones/connectfour/util/Generator.java
+++ src/main/java/com/jesseojones/connectfour/util/Generator.java
@@ -8,12 +8,4 @@ public class Generator {
 		return new char[GameBoard.GAME_BOARD_SPACES];
 	}
 
-	public char[][] generateComplexGameBoard() {
-		final char[][] complexGameBoard = new char[GameBoard.GAME_BOARD_X_AXIS][];
-		for (int i = 0; i < GameBoard.GAME_BOARD_X_AXIS; i++) {
-			complexGameBoard[i] = new char[GameBoard.GAME_BOARD_Y_AXIS];
-		}
-		return complexGameBoard;
-	}
-
 }
