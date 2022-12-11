f7b6d28ee57999eb324a6c50b8c5717f02fbc8fa
jesseojones <jesseoj98@gmail.com>
2022-12-11 16:28:58 -0600

    Result getters & setters

--- src/main/java/com/jesseojones/connectfour/domain/Result.java
+++ src/main/java/com/jesseojones/connectfour/domain/Result.java
@@ -16,4 +16,28 @@ public class Result {
 		this.winningSpaces = winningSpaces;
 	}
 
+	public boolean isCpuWon() {
+		return cpuWon;
+	}
+
+	public void setCpuWon(boolean cpuWon) {
+		this.cpuWon = cpuWon;
+	}
+
+	public boolean isPlayerWon() {
+		return playerWon;
+	}
+
+	public void setPlayerWon(boolean playerWon) {
+		this.playerWon = playerWon;
+	}
+
+	public List<Integer> getWinningSpaces() {
+		return winningSpaces;
+	}
+
+	public void setWinningSpaces(List<Integer> winningSpaces) {
+		this.winningSpaces = winningSpaces;
+	}
+
 }
