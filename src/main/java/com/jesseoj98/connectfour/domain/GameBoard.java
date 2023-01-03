460d577aa87a09f71c5bf9b1ad0f9ec0f0a58524
jesseojones <jesseoj98@gmail.com>
2023-01-03 08:43:49 -0600

    add more constants to game board class

--- src/main/java/com/jesseoj98/connectfour/domain/GameBoard.java
+++ src/main/java/com/jesseoj98/connectfour/domain/GameBoard.java
@@ -2,23 +2,39 @@ package com.jesseoj98.connectfour.domain;
 
 public class GameBoard {
 
+	public static final int TOP_LEFT = -8;
+
+	public static final int BELOW = -7;
+
+	public static final int TOP_RIGHT = -6;
+
+	public static final int LEFT = -1;
+
+	public static final int RIGHT = 1;
+
 	public static final int BESIDE = 1;
 
 	public static final int LOWER_BOUND = 1;
 
 	public static final int CEILING = 6;
 
+	public static final int BOTTOM_LEFT = 6;
+
 	public static final int FORWARD_DIAGONAL = 6;
 
 	public static final int GAME_BOARD_Y_AXIS = 6;
 
+	public static final int ABOVE = 7;
+
 	public static final int LEVEL = 7;
+
+	public static final int PREVIOUS_ROW = 7;
 
 	public static final int UPPER_BOUND = 7;
 
 	public static final int GAME_BOARD_X_AXIS = 7;
 
-	public static final int PREVIOUS_ROW = 7;
+	public static final int BOTTOM_RIGHT = 8;
 
 	public static final int BACKWARD_DIAGONAL = 8;
 
