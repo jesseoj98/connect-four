ef0c9d670d05b1358c2f9b7c86c726d537b41228
jesseojones <jesseoj98@gmail.com>
2023-01-05 09:34:21 -0600

    construct connect four class to start game

--- src/main/java/com/jesseoj98/connectfour/ConnectFour.java
+++ src/main/java/com/jesseoj98/connectfour/ConnectFour.java
@@ -1,10 +1,32 @@
 package com.jesseoj98.connectfour;
 
+import java.util.Scanner;
+
+import com.jesseoj98.connectfour.util.Validator;
+
 public class ConnectFour {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static final Game game = new Game();
+	private static final Scanner scanner = new Scanner(System.in);
+	private static final Validator validator = new Validator();
 
+	public static void main(String[] args) {
+
+		char playAgain;
+
+		do {
+
+			game.playGame();
+
+			do {
+				playAgain = scanner.next().charAt(0);
+			} while (!validator.isInputValid(playAgain));
+
+			System.out.println();
+
+		} while (validator.playAgain(playAgain));
+
+		System.out.println("Thanks for playing!");
 	}
 
 }
